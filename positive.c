dea56c75c0ff74b3f06a25b10bd25e5d4646ffc1
karthiknevercant <karthiknevercant@users.noreply.github.com>
2017-06-14 05:23:27 -0700

    Create positive.c

--- positive.c
+++ positive.c
@@ -5,4 +5,13 @@ int n;
 clrscr();
 printf("enter n:");
 scanf("%d",&n);
+        if(n>0) {
+         printf('the given number is positive");
+         }
+         else if(n<0) {
+         printf("the given number is negative");
+         }
+         else {
+         printf("the given number is zero"):
+         }
 }
